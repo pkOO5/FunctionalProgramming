2ec2317555f4a738474d50e94ec15cb8598cfc3c
Pranay <73628557+pkOO5@users.noreply.github.com>
2023-04-01 20:05:31 -0500

    Remove

--- functional-programming/src/programming/FP01Functional.java
+++ functional-programming/src/programming/FP01Functional.java
@@ -7,13 +7,7 @@ public class FP01Functional {
 	public static void main(String[] args) {
 	List<Integer> numbers = List.of(12,4,9,8,7,10,6,3,78,5,7,4);
 	printEvenNumbersInListFunctional(numbers);
-
-	}
-	
-//	private static boolean isEven(int number) {
-//		return number%2 ==0;
-//	}
-	
+	}	
 	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
 		
 		// Define the behavior when we get a number 
